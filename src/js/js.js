b4c2fbc5f2b96f7041663d01830f37391d71d5a1
jaype10 <jaypemendes@gmail.com>
2022-01-19 12:21:53 -0300

    movimento do jogador

--- src/js/js.js
+++ src/js/js.js
@@ -6,15 +6,70 @@ function start() {
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-    var jogo = {}
+    var jogo = {};
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() *334);
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    $(document).keydown(function(e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e) {
+        jogo.pressionou[e.which] = false;
+    });
     
     jogo.timer = setInterval(loop, 30);
     function loop() {
         movefundo();
+        movejogador();
+        moveinimigo();
     }
 
     function movefundo() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position", esquerda-1);
+    }
+
+    function movejogador() {
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo-10);
+
+            if (topo <= 0) {
+                $("#jogador").css("top", topo+10);
+            }
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo+10);
+
+            if (topo >= 434) {
+                $("#jogador").css("top",topo-10);
+            }
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+            
+        }
+    }
+
+    function moveinimigo1() {
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
     }
 }
